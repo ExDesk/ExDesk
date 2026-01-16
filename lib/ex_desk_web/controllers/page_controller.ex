32efc49e81cd9f04c6cd9d98f91c837d826b295f
Joelbrit0 <joelsonbrito90@gmail.com>
2026-01-16 17:56:55 -0300

    feat: Initialize web router with authentication flows and core UI layouts including flash messages and theme toggle.

--- lib/ex_desk_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule ExDeskWeb.PageController do
-  use ExDeskWeb, :controller
-
-  def home(conn, _params) do
-    render(conn, :home)
-  end
-end
--- lib/ex_desk_web/router.ex
+++ lib/ex_desk_web/router.ex
@@ -17,24 +17,8 @@ defmodule ExDeskWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", ExDeskWeb do
-    pipe_through :browser
-
-    get "/", PageController, :home
-  end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", ExDeskWeb do
-  #   pipe_through :api
-  # end
-
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:ex_desk, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
@@ -64,6 +48,7 @@ defmodule ExDeskWeb.Router do
 
     live_session :current_user,
       on_mount: [{ExDeskWeb.UserAuth, :mount_current_scope}] do
+      live "/", UserLive.Login, :new
       live "/users/register", UserLive.Registration, :new
       live "/users/log-in", UserLive.Login, :new
       live "/users/log-in/:token", UserLive.Confirmation, :new
